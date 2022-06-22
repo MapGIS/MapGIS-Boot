7fe847fcb921e357fbddc84af13715ed56f417b5
xiongbo <xiongbo@mapgis.com>
2022-06-22 12:05:09 +0800

    fix: cloud docker-startup.sh remove spring.config.additional-location

--- mapgis-cloud-packer/package/bin/docker-startup.sh
+++ mapgis-cloud-packer/package/bin/docker-startup.sh
@@ -5,4 +5,4 @@ cd ../
 LIB=$1
 MAPGIS_SERVER_JAR_NAME=$(ls -r lib | grep '^'${LIB}'-[0-9\.]*\.jar$' | head -n 1)
 set JAVA_OPTS=-Xms256m -Xmx1024m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -Dfile.encoding=UTF-8
-java $JAVA_OPTS -jar lib/${MAPGIS_SERVER_JAR_NAME} --spring.profiles.active=prod --spring.config.additional-location=file:config/
+java $JAVA_OPTS -jar lib/${MAPGIS_SERVER_JAR_NAME} --spring.profiles.active=prod
