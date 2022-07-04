242121808a4fa4ceaab2e8c56d5bd0386e1c5c0b
xiongbo <xiongbo@mapgis.com>
2022-07-04 15:42:07 +0800

    feat: add docker package scripts

---
+++ mapgis-local-packer/docker-package/bin/readme.txt
@@ -0,0 +1,27 @@
+---请运行docker version和docker-compose version，确保Docker和Docker Compose已成功安装---
+
+1. 将Docker版的包放到linux服务器的某一目录，如：/opt
+
+2. 安装
+    进入应用根目录
+    执行以下命令，进行安装
+    sudo chmod +x install.sh && sudo ./install.sh
+
+3. 调整参数【必须】
+    编辑.env，根据需要修改相关参数，运行sudo vi .env
+
+    # 私有镜像仓库地址，注意结尾需含/，默认为空
+    DOCKER_REGISTRY_URL=
+
+    # 应用端口
+    MAPGIS_PORT=8080
+
+4、启动/停止
+    启动
+    sudo ./startup.sh
+    停止
+    sudo ./shutdown.sh
+
+5、访问应用
+    http://{DOCKER_HOST_IP}:{MAPGIS_PORT}
+    其中{DOCKER_HOST_IP}为Docker宿主机的IP，{MAPGIS_PORT}为应用端口
---
+++ mapgis-local-packer/docker-package/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+cd `dirname $0`
+
+# 平台名称
+PLATFORM_NAME=$1
+CURRENT_DIR=$(cd `dirname $0`; pwd)
+DOCKER_BUILD_PATH=${CURRENT_DIR}/../docker/${PLATFORM_NAME}
+RELEASE_DIR=${CURRENT_DIR}/../docker-release/${PLATFORM_NAME}
+
+echo "开始构建平台${PLATFORM_NAME} Docker镜像"
+
+# 进入Docker构建目录
+cd $DOCKER_BUILD_PATH
+
+# 赋予执行权限
+chmod +x ./copy.sh  ./copy-server.sh ./deploy.sh
+
+# 准备打包资源
+./copy.sh
+
+# 构建镜像
+./deploy.sh build
+
+echo "开始发布平台${PLATFORM_NAME} Docker镜像"
+
+# 发布镜像
+./deploy.sh publish
+
+echo "开始打包平台${PLATFORM_NAME}"
+
+# 如果存在目标目录先删除
+if [ -d $RELEASE_DIR ]; then
+    rm -rf $RELEASE_DIR
+fi
+
+# 创建目标目录
+mkdir -p $RELEASE_DIR
+
+# 拷贝运行脚本
+cp -r $CURRENT_DIR/bin/* $RELEASE_DIR
+
+# 拷贝Docker-Compose脚本
+cp .env $RELEASE_DIR
+cp docker-compose.yml $RELEASE_DIR
+
+echo "完成打包平台${PLATFORM_NAME}"
