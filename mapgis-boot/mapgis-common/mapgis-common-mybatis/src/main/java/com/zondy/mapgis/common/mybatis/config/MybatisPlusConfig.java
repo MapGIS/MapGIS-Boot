d573026e87a33c965d5ff0d7324f569097aca5f1
xiongbo <xiongbo@mapgis.com>
2023-01-09 15:28:15 +0800

    feat: support mybatis-plus 详细描述: support mybatis-plus

---
+++ mapgis-boot/mapgis-common/mapgis-common-mybatis/src/main/java/com/zondy/mapgis/common/mybatis/config/MybatisPlusConfig.java
@@ -0,0 +1,62 @@
+package com.zondy.mapgis.common.mybatis.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+/**
+ * MyBatis Plus 配置
+ *
+ * @author xiongbo
+ * @since 2023/1/9 10:10
+ */
+@EnableTransactionManagement(proxyTargetClass = true)
+@Configuration
+// 指定要扫描的Mapper类的包的路径
+@MapperScan("com.zondy.mapgis.**.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        interceptor.addInnerInterceptor(paginationInnerInterceptor());
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(optimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+
+    /**
+     * 分页插件，自动识别数据库类型 https://baomidou.com/guide/interceptor-pagination.html
+     */
+    public PaginationInnerInterceptor paginationInnerInterceptor() {
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInnerInterceptor.setMaxLimit(-1L);
+        return paginationInnerInterceptor;
+    }
+
+    /**
+     * 乐观锁插件 https://baomidou.com/guide/interceptor-optimistic-locker.html
+     */
+    public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor() {
+        return new OptimisticLockerInnerInterceptor();
+    }
+
+    @Bean
+    public DatabaseIdProvider getDatabaseIdProvider() {
+        DatabaseIdProvider databaseIdProvider = new VendorDatabaseIdProvider();
+        Properties properties = new Properties();
+        properties.setProperty("MySQL", "mysql");
+        properties.setProperty("SQLite", "sqlite");
+        databaseIdProvider.setProperties(properties);
+        return databaseIdProvider;
+    }
+}
--- mapgis-boot/mapgis-common/mapgis-common-security/mapgis-common-cloud-security/src/main/java/com/zondy/mapgis/common/security/annotation/EnableMapConfig.java
+++ mapgis-boot/mapgis-common/mapgis-common-security/mapgis-common-cloud-security/src/main/java/com/zondy/mapgis/common/security/annotation/EnableMapConfig.java
@@ -2,8 +2,6 @@ package com.zondy.mapgis.common.security.annotation;
 
 import com.zondy.mapgis.common.security.config.ApplicationConfig;
 import com.zondy.mapgis.common.security.feign.FeignAutoConfiguration;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -13,10 +11,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-// 表示通过aop框架暴露该代理对象,AopContext能够访问
-@EnableAspectJAutoProxy(exposeProxy = true)
-// 指定要扫描的Mapper类的包的路径
-@MapperScan("com.zondy.mapgis.**.mapper")
 // 开启线程异步执行
 @EnableAsync
 // 自动加载类
