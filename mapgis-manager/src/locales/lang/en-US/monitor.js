f3f4f12d2061be849b84411039129a4317edec4e
xiongbo <xiongbo@mapgis.com>
2023-01-30 11:11:28 +0800

    style: perfect cache internationalization

--- mapgis-manager/src/locales/lang/en-US/monitor.js
+++ mapgis-manager/src/locales/lang/en-US/monitor.js
@@ -1,5 +1,6 @@
 import online from './monitor/online'
 import server from './monitor/server'
 import performance from './monitor/performance'
+import cache from './monitor/cache'
 
-export default { ...online, ...server, ...performance }
+export default { ...online, ...server, ...performance, ...cache }
