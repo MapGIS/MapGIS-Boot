3585c7d3d6989fb821b4d0d7c280b26c88cdcf7a
xiongbo <xiongbo@mapgis.com>
2023-07-26 18:42:16 +0800

    feat: add micro app template

---
+++ mapgis-manager-extension/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const initRouter = () => {
+  const options = {
+    mode: 'history',
+    base: window.__POWERED_BY_QIANKUN__ ? '/xxx/manager/extension/' : '/',
+    routes
+  }
+
+  return new VueRouter(options)
+}
+
+export { initRouter }
---
+++ mapgis-manager-extension/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="MapGIS logo" src="../assets/logo-title.svg" style="width: 300px" />
+    <h1>Micro App Template</h1>
+    <br />
+    <a-date-picker />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
