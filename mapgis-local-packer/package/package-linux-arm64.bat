9f6f856b9372011db380efa856265e5a33e69e02
xiongbo <xiongbo@mapgis.com>
2023-09-27 11:03:24 +0800

    style: fix syntax error

--- mapgis-local-packer/package/package-linux-arm64.bat
+++ mapgis-local-packer/package/package-linux-arm64.bat
@@ -14,7 +14,7 @@ if "%SKIP_FRONT%" == "" set SKIP_FRONT=""
 set COMPILE_FORNT_BACK=true
 if "%~2" == "SKIP_FRONT_BACK" set COMPILE_FORNT_BACK=false
 
-set OUTPUT_DIR="%ROOT_DIR_REV%/../output
+set OUTPUT_DIR=%ROOT_DIR_REV%/../output
 if "%~3" neq "" set "OUTPUT_DIR=%~3"
 
 if "%COMPILE_FORNT_BACK%" == "true" (
